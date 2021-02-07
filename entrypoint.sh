6028aeedcc07f8c5a7c4b5a922d47ba20e44d4a1
Fabio Bonelli <fb@fabiobonelli.it>
2021-02-07 19:24:20 +0100

    wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ echo $GITHUB_ACTIONS
 echo Executing pcvalidate
 pcvalidate "$1"
 
-pcvalidate "$1" | reviewdog -efm="$1:%f:%l:%c: %m" -reporter=github-pr-review -tee
+pcvalidate "$1" | reviewdog -efm="%f:%l:%c: %m" -reporter=github-pr-review -tee
