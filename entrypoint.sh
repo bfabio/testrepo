0daabdda7f9377ed96c0494714684d5066ec4696
Fabio Bonelli <fb@fabiobonelli.it>
2021-02-09 20:38:59 +0100

    wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,9 @@
 
 set -o pipefail
 
-pcvalidate "$1" | reviewdog -efm="%f:%l:%c: %m" -reporter=github-pr-review -tee
+# comment-on-pr parameter from the action
+if [ "$2" == "true" ]; then
+    pcvalidate "$1" | reviewdog -efm="%f:%l:%c: %m" -reporter=github-pr-review -tee
+else
+    pcvalidate "$1"
+fi
